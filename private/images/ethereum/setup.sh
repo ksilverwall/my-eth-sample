c685e29cfddd35c42bd51925e6a2f90f2d57dc8b
Kazuki Tsunemi <ksilverwall@gmail.com>
2023-03-07 02:16:07 +0900

    auto init if dir not exists

--- private/images/ethereum/setup.sh
+++ private/images/ethereum/setup.sh
@@ -3,5 +3,5 @@ for I in "${@}"; do
   echo === setup $I ===
   rm -rf $I
   mkdir -p $I || exit -1
-  geth init --datadir $I ${FILE:-./files/shared/genesis.json} || exit -1
+  geth init --datadir $I ${GENESIS_FILE:-./files/shared/genesis.json} || exit -1
 done
